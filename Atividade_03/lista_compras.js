df113635d7a227f3282114a48e04cdb5a0342a48
Luiza Gomes <loyxeschool91@gmail.com>
2026-03-12 12:16:48 -0300

    Create lista_compras.js

---
+++ Atividade_03/lista_compras.js
@@ -0,0 +1,10 @@
+let lista_compras = ["Arroz","Feijão","Açúcar","Ovos"];
+lista_compras.forEach(item =>{
+    console.log(item);
+});   
+
+let cesta_frutas = ["Uva", "Morango", "Maçã", "Melão"];
+cesta_frutas.push ("Banana");
+cesta_frutas.forEach (item =>{
+    console.log(item);
+});
